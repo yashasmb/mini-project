5a9e6766b3fd10969daf58f1c6f87457ebcc7561
Yashas M B <yashasmb2003@gmail.com>
2024-05-27 20:05:59 +0530

    created another templates folder
    created another template folder inside the base app and added the home and rooms html files

--- threadhive/base/views.py
+++ threadhive/base/views.py
@@ -11,11 +11,11 @@ rooms=[
 
 def home(request):
     context = {'rooms':rooms}
-    return render(request,'home.html' ,  context)
+    return render(request,'base/home.html' ,  context)
     
 
 
 def room(request):
-    return render(request,'rooms.html')
+    return render(request,'base/rooms.html')
     
     
