a7ea244c7071b5d786d02cfc9633a82e1dae18d2
Yashas M B <yashasmb2003@gmail.com>
2024-05-27 20:23:36 +0530

    added dynamic templating

--- threadhive/base/urls.py
+++ threadhive/base/urls.py
@@ -6,5 +6,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name= "home"),
-    path('room/', views.room, name= 'room'),
+    path('room/<str:pk>', views.room, name= 'room'),
 ]
--- threadhive/base/views.py
+++ threadhive/base/views.py
@@ -15,7 +15,13 @@ def home(request):
     
 
 
-def room(request):
-    return render(request,'base/rooms.html')
+def room(request,pk):
+    room = None
+    for i in rooms:
+        if i['id']==int(pk):
+            room = i
+            
+    context ={'room':room}
+    return render(request,'base/rooms.html',context)
     
     
