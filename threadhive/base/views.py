8962f9619626f26c82d81b77e02909c520600bf6
Yashas M B <yashasmb2003@gmail.com>
2024-05-27 20:01:26 +0530

    added templates folder
    added a separate folder which has all the templates

--- threadhive/base/views.py
+++ threadhive/base/views.py
@@ -3,11 +3,19 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+rooms=[
+    {'id':1,'name': "Lets learn python!"},
+    {'id':2,'name': "design with me"},
+    {'id':3,'name': "front-end developer"},
+]
 
-def home(requests):
-    return HttpResponse("home page")
+def home(request):
+    context = {'rooms':rooms}
+    return render(request,'home.html' ,  context)
+    
 
 
 def room(request):
-    return HttpResponse("<title>Room page</title>Room page")
+    return render(request,'rooms.html')
+    
     
--- threadhive/threadhive/settings.py
+++ threadhive/threadhive/settings.py
@@ -56,7 +56,8 @@ ROOT_URLCONF = 'threadhive.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
