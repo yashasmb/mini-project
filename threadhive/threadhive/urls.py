3f06ea7c35c799a2b1edb73dbdf397d2484435b7
Yashas M B <yashasmb2003@gmail.com>
2024-07-06 10:10:52 +0530

    added another user

--- threadhive/threadhive/urls.py
+++ threadhive/threadhive/urls.py
@@ -2,8 +2,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-# username:yashas
-# password:yashas123
+#username:yashas
+#password:yashas123
 
 #username:sooraj
 #password:sooraj123
@@ -12,6 +12,10 @@ from django.urls import path, include
 
 #username: sanskar
 # password: bit@2025
+
+
+#username: ashishankam
+#password: sobha1074
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
